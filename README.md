dd9b88616a1367c46798ce765b02d4eb4c7a08c5
Bron10 <bron1010@gmail.com>
2020-07-13 18:11:11 +0530

    Modal selects a focusable element

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Simple react modal
 1. To install `npm install`
 2. To run `npm run start-example`
 3. To run with example, go to `http://localhost:1234/`
+
+
+# Todo
+1. Test, with complex components
+2. All input elements focusable
+3. focus back to open button
--- example/index.js
+++ example/index.js
@@ -6,32 +6,65 @@ class HelloMessage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: false
+      show: false,
+      showForm : false
     };
     this.openModal = this.openModal.bind(this);
     this.closeModal = this.closeModal.bind(this);
+    this.openFormModal = this.openFormModal.bind(this);
   }
   
   closeModal(){
     this.setState({
-      show : false
+      show : false,
+      showForm : false
     })
   }
 
   openModal(){
     this.setState({
-      show : true
+      show : true,
+    })
+  }
+  
+  openFormModal(){
+    this.setState({
+      showForm : true
     })
   }
 
   render() {
-    const {show} = this.state;
+    const {show, showForm} = this.state;
     return (
     <>
       <button onClick= {this.openModal}>Open Modal</button>
+      <button onClick = {this.openFormModal}>Open form modal</button>
       <Modal display={show}>
-        <div onClick={this.closeModal}>[x]</div>
-        <div>Hello {this.props.name}, click on above [x] to close it</div>
+        <h2>Modal Window</h2>
+        <div className="content">
+          Hello everyone
+        </div>
+        <div className="actions">
+          <button onClick={this.closeModal} className="toggle-button">
+            close
+          </button>
+        </div>
+      </Modal>
+      <Modal display={showForm}>
+        <h2>Modal Form Window</h2>
+        <div className="content">
+          <form>
+            <label htmlFor="fname">First name:</label><br />
+            <input type="text" id="fname" name="fname" defaultValue="" tabIndex="0"/><br />
+            <label htmlFor="lname">Last name:</label><br />
+            <input type="text" id="lname" name="lname" defaultValue="" /><br />
+          </form> 
+        </div>
+        <div className="actions">
+          <button onClick={this.closeModal} className="toggle-button">
+            close
+          </button>
+        </div>
       </Modal>
     </>)
   }
